7e1b163684dc5eac86c4677c6d52bc98ad211525
danduke99 <72228945+danduke99@users.noreply.github.com>
2021-04-18 15:59:04 -0400

    Create AccountProfile.js

---
+++ src/components/account/AccountProfile.js
@@ -0,0 +1,74 @@
+import moment from 'moment';
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Typography
+} from '@material-ui/core';
+
+const user = {
+  avatar: '/static/avatar.jpg',
+  city: 'Medellin',
+  country: 'Colombia',
+  jobTitle: 'Student',
+  faculty: 'Ingenieria',
+  carrera: 'Ingenieria de Sistemas',
+  name: 'Deelan Duffis',
+  timezone: 'GMT-5'
+};
+
+const AccountProfile = (props) => (
+  <Card {...props}>
+    <CardContent>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <Avatar
+          src={user.avatar}
+          sx={{
+            height: 100,
+            width: 100
+          }}
+        />
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h3"
+        >
+          {user.name}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {`${user.carrera}`}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {`${moment().format('hh:mm A')} ${user.timezone}`}
+        </Typography>
+      </Box>
+    </CardContent>
+    <Divider />
+    <CardActions>
+      <Button
+        fullWidth
+        variant="text"
+      >
+        Cambiar Foto
+      </Button>
+    </CardActions>
+  </Card>
+);
+
+export default AccountProfile;
