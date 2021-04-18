61bfb07c2439341bde171f518728ea11e353e2ef
danduke99 <72228945+danduke99@users.noreply.github.com>
2021-04-18 15:59:17 -0400

    Add files via upload

---
+++ src/components/account/AccountProfileDetails.js
@@ -0,0 +1,143 @@
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  TextField
+} from '@material-ui/core';
+
+const AccountProfileDetails = (props) => {
+  const [values, setValues] = useState({
+    firstName: 'Deelan',
+    lastName: 'Duffis',
+    email: 'front@UPI.co',
+    phone: '123 456 7890',
+    country: 'Colombia'
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      {...props}
+    >
+      <Card>
+        <CardHeader
+          subheader="Puedes editar tu informacion."
+          title="Tu Perfil"
+        />
+        <Divider />
+        <CardContent>
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                helperText="Please specify the first name"
+                label="First name"
+                name="firstName"
+                onChange={handleChange}
+                required
+                value={values.firstName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Last name"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={values.lastName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Email Address"
+                name="email"
+                onChange={handleChange}
+                required
+                value={values.email}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Phone Number"
+                name="phone"
+                onChange={handleChange}
+                value={values.phone}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Country"
+                name="country"
+                onChange={handleChange}
+                required
+                value={values.country}
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            p: 2
+          }}
+        >
+          <Button
+            style={{ background: '#508ff4' }}
+            variant="contained"
+          >
+            Save details
+          </Button>
+        </Box>
+      </Card>
+    </form>
+  );
+};
+
+export default AccountProfileDetails;
